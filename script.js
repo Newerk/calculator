892e06127b39aa0c800325f2a4c2b84265033813
newerk <juniusowens@yahoo.com>
2022-07-11 16:30:40 -0400

    Add Working Negative Toggle Button
    When the user presses the negative number, the number displayed on the
    calculator will either had a - in the front if there isnt one already,
    or remove it if there is one already

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-var input1 = 0;
-var input2 = 0; 
+var displayValue = 0;
+var num1 = 0;
+var num2 = 0;
+
 var display = document.querySelector('#display');
 
 //When number buttons are pressed, it is displayed on the calc in the order they were selected
@@ -7,44 +9,90 @@ let numButtons = document.querySelectorAll('.numbers');
 numButtons.forEach(btn => {
     btn.addEventListener('click', e => {
         display.textContent += e.target.textContent;
-        input1 = parseInt(display.textContent);
-        // console.log(input1);
+        displayValue = parseInt(display.textContent);
+        // console.log(displayValue);
+        // console.log(displayValue.toString()[3])
     })
+
 });
 
-let clearButton = document.querySelector('#clear');
-clearButton.addEventListener('click', clear);
+// let operatorButtons = document.querySelectorAll('.operators');
+// operatorButtons.forEach(btn => {
+//     btn.addEventListener('click', e => {
+//         let x = displayValue;
+//         console.log(num1);
+
+        // switch (e.target.id) {
+        //     case "add":
+
+        //         break;
+        //     case "subtract":
+
+        //         break;
+        //     case "multiply":
+
+        //         break;
+        //     case "divide":
+
+        //         break;
+        //     case "power":
+
+        //         break;
+
+        //     default:
+        //         break;
+        // }
+//     })
+// })
+
+let negBtn = document.querySelector('#pos-or-neg');
+negBtn.addEventListener('click', negativeToggle);
+
+let clearBtn = document.querySelector('#clear');
+clearBtn.addEventListener('click', clear);
 
 
-const add = (num1, num2) => num1 + num2;
-const subtract = (num1, num2) => num1 - num2;
-const multiply = (num1, num2) => num1 * num2;
-const divide = (num1, num2) => num1 / num2;
-const power = (num1, num2) => Math.pow(num1, num2);
 
 
-const operate = (operator, num1, num2) => {
+const add = (x, y) => x + y;
+const subtract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
+const power = (x, y) => Math.pow(x, y);
+
+
+const operate = (operator, x, y) => {
 
     switch (operator) {
         case add:
-            display.textContent = add(num1, num2);
+            clear();
+            num2 = displayValue;
+            display.textContent = add(x, num2);
 
             break;
         case subtract:
-            display.textContent = subtract(num1, num2);
+            clear();
+            num2 = displayValue;
+            display.textContent = subtract(x, y);
 
             break;
         case multiply:
-            display.textContent = multiply(num1, num2);
+            clear();
+            num2 = displayValue;
+            display.textContent = multiply(x, y);
 
             break;
         case divide:
-            display.textContent = divide(num1, num2);
+            clear();
+            num2 = displayValue;
+            display.textContent = divide(x, y);
 
             break;
 
         case power:
-            display.textContent = power(num1, num2);
+            clear();
+            num2 = displayValue;
+            display.textContent = power(x, y);
 
             break;
 
@@ -56,10 +104,31 @@ const operate = (operator, num1, num2) => {
     return display.textContent;
 };
 
+
+function negativeToggle() {
+    let value = displayValue.toString();
+
+    if (value[0] !== '-') {
+        display.textContent = `-${value}`;
+        displayValue = display.textContent;
+        
+    }
+
+    if (value[0] === '-') {
+        display.textContent = value.slice(1, value.length);
+        displayValue = display.textContent
+
+    }
+    // console.log(displayValue);
+}
+
+
+
 //clears calc display and reset all values to defaults
 function clear() {
-    input1 = 0;
-    input2 = 0;
+    displayValue = 0;
+    num1 = 0;
+    num2 = 0;
     display.textContent = '';
 }
 
