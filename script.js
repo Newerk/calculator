b78fdfde5a78717a2d7ef482eae8e6b79a2936a5
newerk <juniusowens@yahoo.com>
2022-07-11 09:33:19 -0400

    Add All Calc Btns and Four Functions for Operands
    Added HTML for all buttons that will function on this calculator app.
    So far I have created functions for adding, subtracting, multiplying
    and dividing.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const add = (num1, num2) => num1 + num2;
 
+const subtract = (num1, num2) => num1 - num2;
+
+const multiply = (num1, num2) => num1 * num2;
+
+const divide = (num1, num2) => num1 / num2;
+
+const operate = (operator, num1, num2) => {
+    
+};
+
+
+
+
+
+console.log(add(1,2));//3
+
+console.log(subtract(2,1));//1
+
+console.log(multiply(2,2));//4
+
+console.log(divide(4,2));//2
