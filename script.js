00fda2c71e5e07c71ec508ae5b4bf3ac33ab1107
newerk <juniusowens@yahoo.com>
2022-07-12 12:40:56 -0400

    Add Functioning Equals Btn That Wrks W/ 2 Inputs
    Anything past 2 inputs from the user doesnt calculate, but so far an
    expression with only 1 operator properly calculates

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ console.log(numberIsDecimal);
 
 var display = document.querySelector('#display');
 
+const add = (x, y) => x + y;
+const subtract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
+const power = (x, y) => Math.pow(x, y);
+
 //When number buttons are pressed, it is displayed on the calc in the order they were selected
 let numButtons = document.querySelectorAll('.numbers');
 numButtons.forEach(btn => {
@@ -16,44 +22,114 @@ numButtons.forEach(btn => {
 
 });
 
-// let operatorButtons = document.querySelectorAll('.operators');
-// operatorButtons.forEach(btn => {
-//     btn.addEventListener('click', e => {
+let operatorButtons = document.querySelectorAll('.operators');
+operatorButtons.forEach(btn => {
+    btn.addEventListener('click', e => {
 
-//     })
-// })
+        switch (e.target.id) {
+            case 'add':
+                storeNumOne();
+                num1 += '+';
 
 
-//there is an unintended interaction with the decimal button. when there is a decimal in the display, pressing the negative button removes the decimal
+                break;
+            case 'subtract':
+                storeNumOne();
+
+                num1 += '-';
+
+
+                break;
+            case 'multiply':
+                storeNumOne();
+
+                num1 += '*';
+
+
+                break;
+            case 'divide':
+                storeNumOne();
+
+                num1 += '/';
+
+
+                break;
+            case 'exponent':
+                storeNumOne();
+                num1 += '^';
+
+
+
+                break;
+            case 'equal':
+                let getOperator = num1.toString().slice(num1.toString().length - 1, num1.toString().length);
+                num2 = display.textContent = displayValue;
+                num1 = parseInt(num1);
+                num2 = parseInt(num2);
+
+                switch (getOperator) {
+                    case '+': 
+                        operate('+', num1, num2);
+
+                        break;
+                    case '-':
+                        operate('-', num1, num2);
+
+                        break
+                    case '*':
+                        operate('*', num1, num2);
+
+                        break
+                    case '/':
+                        operate('/', num1, num2);
+
+                        break
+                    case '^':
+                        operate('^', num1, num2);
+
+                        break
+                }
+
+
+                break;
+
+            default:
+                alert('clicked on the whitespace')
+                break;
+        }
+    })
+})
+
+
 let miscButtons = document.querySelectorAll('.misc');
 miscButtons.forEach(btn => {
     btn.addEventListener('click', e => {
-        if (e.target.id === 'decimal') {
-            addDecimal();
 
-        } else if (e.target.id === 'negative') {
-            negativeToggle();
+        switch (e.target.id) {
+            case 'decimal':
+                addDecimal();
+                break;
 
-        } else if (e.target.id === 'clear') {
-            clear();
+            case 'negative':
+                negativeToggle();
+                break;
 
-        } else if (e.target.id === 'delete') {
-            backspace();
+            case 'clear':
+                clear();
+                break;
+
+            case 'delete':
+                backspace();
+                break;
 
         }
     });
 });
 
-
-
-const add = (x, y) => x + y;
-const subtract = (x, y) => x - y;
-const multiply = (x, y) => x * y;
-const divide = (x, y) => x / y;
-const power = (x, y) => Math.pow(x, y);
-
-
 const operate = (operator, x, y) => {
+    // let result = 0;
+    parseInt(num1);
+    parseInt(num2);
 
     switch (operator) {
         case '+':
@@ -79,31 +155,39 @@ const operate = (operator, x, y) => {
             break;
 
         default:
-            alert('Not an operator');
+            alert('clicked on the whitespace');
             break;
     }
 
-    return display.textContent;
 };
+
+function storeNumOne() {
+    num1 = displayValue;
+    display.textContent = displayValue = '';
+    // console.log(num1);
+}
+
+// function removeOperator() {
+//     num1.
+    
+// }
 
 function negativeToggle() {
     let value = displayValue;
 
     if (value[0] !== '-') {
-        display.textContent = `-${value}`;
-        displayValue = display.textContent;
+        display.textContent = displayValue = `-${value}`;
+
 
     } else if (value[0] === '-') {
-        display.textContent = value.slice(1, value.length);
-        displayValue = display.textContent
+        display.textContent = displayValue = value.slice(1, value.length);
 
     }
 }
 
 function addDecimal() {
     if (numberIsDecimal === false) {
-        displayValue += '.';
-        display.textContent = displayValue;
+        display.textContent = displayValue += '.';
         numberIsDecimal = true;
     }
 }
@@ -111,8 +195,7 @@ function addDecimal() {
 //deletes the n'th value of the number shown on the display. One more thing I want to do is have the entire display get cleared if a solution is returned after the equals button is clicked.
 //if this deletes a decimal, it should reset numberIsDecimal to false so that the decimal can be reapplied whenever the user wants
 function backspace() {
-    displayValue = displayValue.toString().slice(0, displayValue.toString().length - 1);
-    display.textContent = displayValue;
+    display.textContent = displayValue = displayValue.slice(0, displayValue.length - 1);
 
     if (displayValue.includes('.')) {
         numberIsDecimal = true;
